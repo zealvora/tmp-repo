7d562e2a1204fe964e0b38f1736a49b2c7c81745
Zeal Vora <instructors@kplabs.in>
2020-09-07 12:54:40 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,20 +1,9 @@
 locals {
-  arr = ["host1", "host2", "host3"]
+  arr = ["dev1", "dev22", "dev3"]
 }
 
 
-locals {
-  arr2 = ["host1", "host2", "host3"]
-}
-
-locals {
-  arr3 = ["host1", "host2", "host3"]
-}
 
 output "test" {
   value = local.arr
-}
-
-output "tes2" {
-  value = local.arr3
 }
